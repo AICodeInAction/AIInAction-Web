beb0abe33ce227b760a8f61823bf0b336fbceeea
Jason <superjavason@gmail.com>
2026-02-20 14:43:33 +0800

    fix tsc

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,13 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { learningPaths, challenges } from "../src/data/challenges";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   console.log("Seeding database...");
